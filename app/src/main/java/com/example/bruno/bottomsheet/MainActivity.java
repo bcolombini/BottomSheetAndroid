3c3f7157955af452b867087c0df093884689d57a
Bruno Colombini <bruno.colombini@m4u.com.br>
2016-08-08 10:37:34 -0300

    Update Material Design

--- app/src/main/java/com/example/bruno/bottomsheet/MainActivity.java
+++ app/src/main/java/com/example/bruno/bottomsheet/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bruno.bottomsheet;
 
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,49 +13,26 @@ import android.widget.LinearLayout;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private BottomSheetBehavior mBottomSheetBehavior;
+    private BottomSheetDialogFragment bottomSheetDialogFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View bottomSheet = findViewById(R.id.bottom_sheet);
+        bottomSheetDialogFragment = new TutsPlusBottomSheetDialogFragment();
 
-        Button button1 = (Button) findViewById(R.id.button_1);
-        Button button2 = (Button) findViewById(R.id.button_2);
-        Button button3 = (Button) findViewById(R.id.button_3);
+        Button bt1 = (Button) findViewById(R.id.button_1);
+        bt1.setOnClickListener(this);
 
-        LinearLayout LinearButton1 = (LinearLayout) findViewById(R.id.save);
-        LinearLayout LinearButton2 = (LinearLayout) findViewById(R.id.share);
 
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-
-        //Buttons ButtomSheet
-        LinearButton1.setOnClickListener(this);
-        LinearButton2.setOnClickListener(this);
-
-        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
     }
 
     @Override
     public void onClick(View view) {
         switch( view.getId() ) {
             case R.id.button_1: {
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                break;
-            }
-            case R.id.button_2:{
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                break;
-            }
-            case R.id.save:{
-                Log.d("Teste: ","BIRLL!");
-                break;
-            }
-            case R.id.share:{
-                Log.d("Teste: ","Ta saindo da jaula!");
+                bottomSheetDialogFragment.show(getSupportFragmentManager(), "bottom sheet");
                 break;
             }
         }
---
+++ app/src/main/java/com/example/bruno/bottomsheet/TutsPlusBottomSheetDialogFragment.java
@@ -0,0 +1,46 @@
+package com.example.bruno.bottomsheet;
+
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.Log;
+import android.view.View;
+
+public class TutsPlusBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    static BottomSheetDialogFragment newInstance() {
+        return new BottomSheetDialogFragment();
+    }
+
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
+
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                Log.d("Teste: ","Testes");
+                dismiss();
+            }
+
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.bottom_sheet_layout, null);
+        dialog.setContentView(contentView);
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) contentView.getParent()).getLayoutParams();
+        CoordinatorLayout.Behavior behavior = params.getBehavior();
+
+        if( behavior != null && behavior instanceof BottomSheetBehavior ) {
+            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+            ((BottomSheetBehavior) behavior).setState(BottomSheetBehavior.STATE_EXPANDED);
+        }
+    }
+}
